3dac571abf1632d3afb8cd3d3a8b0d078071ebca
Larry Osborn <larry@cornsyrup.org>
2020-09-03 15:18:29 -0400

    housekeeping

--- vim-bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export CWD=`pwd`
-echo $CWD/.vimrc
-rm -rf ~/.vim/bundle/Vundle.vim
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-rm -rf ~/.vimrc ~/.vim
-ln -s $CWD/.vimrc ~/
